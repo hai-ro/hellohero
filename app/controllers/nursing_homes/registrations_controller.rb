19826969ae761497360ce6da4b64bc26ef5d7e93
EC2 Default User <ec2-user@ip-172-31-21-2.ap-northeast-1.compute.internal>
2021-01-28 12:02:43 +0000

    事業所側の設定完了（簡易）

--- app/controllers/nursing_homes/registrations_controller.rb
+++ app/controllers/nursing_homes/registrations_controller.rb
@@ -5,29 +5,29 @@ class NursingHomes::RegistrationsController < Devise::RegistrationsController
   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
-  # def destroy
-  #   super
-  # end
+  def destroy
+    super
+  end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
@@ -42,21 +42,30 @@ class NursingHomes::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :name, :address, :contact_person, :phone_number
+      ])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :name, :address, :contact_person, :phone_number,
+      :website
+      ])
   end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    super(resource)
+    nursing_home_path(resource)
   end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    super(resource)
+  end
+  
+  def after_update_path_for(resource)
+    nursing_home_path(resource)
+  end
 end
